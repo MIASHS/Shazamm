1b694630bedd076cc222a0c073a73b1118eb08e9
Gab55 <gabrielgiroud@gmail.com>
2017-02-25 00:22:48 +0100

    changements mineurs

---
+++ src/com/company/Carte.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gaby on 24/02/2017.
+ */
+public class Carte {
+
+    private int numCarte;
+    private String nomCarte;
+
+    public Carte(int numCarte, String nomCarte) {
+        this.numCarte = numCarte;
+        this.nomCarte = nomCarte;
+    }
+
+    public int getNumCarte() {
+        return numCarte;
+    }
+
+    public void setNumCarte(int numCarte) {
+        this.numCarte = numCarte;
+    }
+
+    public String getNomCarte() {
+        return nomCarte;
+    }
+
+    public void setNomCarte(String nomCarte) {
+        this.nomCarte = nomCarte;
+    }
+}
---
+++ src/com/company/Jeu.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by Gaby on 24/02/2017.
+ */
+public class Jeu {
+
+
+}
