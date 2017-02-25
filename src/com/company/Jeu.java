0cfe8ee478eed46ed71fedda51f0f033450d7622
Gab55 <gabrielgiroud@gmail.com>
2017-02-25 01:25:31 +0100

    changement

--- src/com/company/Jeu.java
+++ src/com/company/Jeu.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
  * Created by Gaby on 24/02/2017.
  */
 public class Jeu {
 
+    private ArrayList<Carte> carteJ1;
+    private ArrayList<Carte> carteJ2;
+    private int nbJoueus;
 
 }
